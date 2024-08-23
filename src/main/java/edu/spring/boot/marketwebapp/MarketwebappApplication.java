d6625fe744cc86f56811be43c4adca5bb48c0275
williamw04 <williamwu120@gmail.com>
2024-08-23 16:53:51 -0400

    java structure for spring boot project

---
+++ src/main/java/edu/spring/boot/marketwebapp/MarketwebappApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.boot.marketwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/spring/boot/marketwebapp/MarketwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.boot.marketwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
